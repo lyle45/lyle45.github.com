6ca0fe6491fe07aa70108090e391307e49881315
lyle45 <lylelamed45@gmail.com>
2016-04-04 18:28:09 +0300

    Added files via upload

--- js/notesController.js
+++ js/notesController.js
@@ -1,8 +1,10 @@
-app.controller('notesController', function($scope) {
+app.controller('notesController', ['$scope' , function($scope) {
 	// localStorage.clear();
 	$scope.saved = localStorage.getItem('notes');
 	$scope.notesArray = (localStorage.getItem('notes') !== null) ? JSON.parse($scope.saved):[];
+	$scope.dim = false; // dim background on edit
 	clearAndFocus();
+
 	$scope.addButton = function() {
 			if ($scope.title == '' || $scope.when == '' || $scope.description == ''){
 				alert("Please fill in all the neccessary details.");
@@ -18,11 +20,18 @@ app.controller('notesController', function($scope) {
 				};
 			};
 
+	function addIfEnterEdit(event, note){
+				if (event.keyCode == 13) {
+					$scope.editNote(note);
+				};
+			};
+
 	$scope.createNote = function(title, when, description) {
 		var note = {};
 		note.title = title;
 		note.when = when;
 		note.description = description;
+		note.inEdit = false;
 		var local = (localStorage.getItem('notes') !== null) ? JSON.parse(localStorage.getItem('notes')) : []; //set localStorage if exists,else start from an empty array
 	    localStorage.clear();
 	    local.push(note);
@@ -45,4 +54,22 @@ app.controller('notesController', function($scope) {
 	    local.splice(i, 1);
 	    localStorage.setItem('notes', JSON.stringify(local));
 	}
-});
+
+	$scope.editNote = function(note) {
+		note.inEdit = true;
+		$scope.dim = true;
+	}
+	$scope.saveEditedNote = function(note) {
+		if (note.title == '' || note.when == '' || note.description == ''){
+				alert("Please fill in all the neccessary details.");
+			} else {
+		note.inEdit = false;
+		$scope.dim = false;
+		var i = $scope.notesArray.indexOf(note);		//edit from localStorage
+		var local = JSON.parse(localStorage.getItem('notes')); 
+	    localStorage.clear();
+	    local[i] = note;
+	    localStorage.setItem('notes', JSON.stringify(local));
+		};
+	};
+}]);
